d1c1ab097c85203daebb58ad1cd3755213d0faba
Andreas Flyvholm Haagh <and.haagh538@gmail.com>
2019-11-12 14:13:17 +0100

    Went back to the old way of checking the cpu

--- PythonApplication1/PythonApplication1/CPU.py
+++ PythonApplication1/PythonApplication1/CPU.py
@@ -2,5 +2,12 @@ import psutil
 import time
 
 def printCpuUsage():
-    percentage = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
-    print(percentage)
+    index = 0
+    while index < 6:
+        print(psutil.cpu_percent(interval = 1, percpu = True))
+        index += 1
+    else:
+        print("The loop has ended")
+
+#percentage = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
+#print(percentage)
