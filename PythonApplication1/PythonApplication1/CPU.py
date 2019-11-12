fc11462f4059a7d1856caf0b636b9ea3d900e661
Andreas Flyvholm Haagh <and.haagh538@gmail.com>
2019-11-12 13:30:39 +0100

    Changed the CPU Percentage code

--- PythonApplication1/PythonApplication1/CPU.py
+++ PythonApplication1/PythonApplication1/CPU.py
@@ -4,7 +4,8 @@ import time
 def printCpuUsage():
     i = 0
     while i < 6:
-        print(psutil.cpu_percent(interval = 1, percpu = True))
+        print(psutil.cpu_percent(interval = 1))
         i += 1
+        #time.sleep(.100)
     else:
         print("the while loop has ended!")
