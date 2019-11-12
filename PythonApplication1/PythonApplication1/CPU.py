523b46ec84a0520a6b9139c78c19be66e11e046a
Andreas Flyvholm Haagh <and.haagh538@gmail.com>
2019-11-12 14:08:11 +0100

    Auto stash before merge of "master" and "origin/master"

--- PythonApplication1/PythonApplication1/CPU.py
+++ PythonApplication1/PythonApplication1/CPU.py
@@ -2,10 +2,5 @@ import psutil
 import time
 
 def printCpuUsage():
-    i = 0
-    while i < 6:
-        print(psutil.cpu_percent(interval = 1, percpu = True))
-        i += 1
-        #time.sleep(.100)
-    else:
-        print("the while loop has ended!")
+    percentage = [x / psutil.cpu_count() * 100 for x in psutil.getloadavg()]
+    print(percentage)
