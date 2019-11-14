b3c46dc8b5ecfb8c19076fc50927aea6f2417a2b
Olga <nimfaolya@mail.ru>
2019-11-14 09:20:20 +0100

    fixed funktion

--- PythonApplication1/PythonApplication1/MemoryControll.py
+++ PythonApplication1/PythonApplication1/MemoryControll.py
@@ -9,19 +9,20 @@ import FileControll
 """
 def printMemoryUsage():
     print ('\n Memory:')
-    print(psutil.virtual_memory()) # printing named tuple
+    data = psutil.virtual_memory()
+    print(data) # printing named tuple
     #printing received information in human readable format
     #Total physical memory in MB
-    memory = psutil.virtual_memory().total >> 20
+    memory = data.total >> 20
     print('Total memory: ', memory, 'MB')
     #Used memory in MB
-    memory = psutil.virtual_memory().used >> 20
+    memory = data.used >> 20
     print('Used: ', memory, 'MB')
     #Memory not being used at all that is readibally avalaible    
-    memory = psutil.virtual_memory().free >> 20
+    memory = data.free >> 20
     print('Free: ', memory, 'MB')
     #Saving data into the log fill
     # via casting into the string format and
     #calling SaveLogFile function from FileControll
-    data = str(psutil.virtual_memory())
+    data = str(data)
     FileControll.SaveLogFile(data)
